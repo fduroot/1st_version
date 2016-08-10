25a55d6fd1d79ca69701af267cd601bd10ac26be
billy191 <14302010007@fudan.edu.cn>
2016-08-10 14:31:47 +0800

    show commentNum on index

--- php/getHottest.php
+++ php/getHottest.php
@@ -24,7 +24,7 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$i += 1;
 }
 $arr['passage']['slide']=$i;*/
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='sell' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='sell' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -35,12 +35,13 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['sell'][$i]['count']=$row['browsed'];
 	$arr['sell'][$i]['time']=$row['created_time'];
 	$arr['sell'][$i]['nickname']=$row['nickname'];
+	$arr['sell'][$i]['commentNum']=$row['comments'];
 	//$arr['sell'][$i]['img']=$row['picture'];
 	$arr['sell'][$i]['title']=$row['title'];
 	$i += 1;
 }
 $arr['passage']['sell']=$i;
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='buy' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='buy' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -51,12 +52,13 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['buy'][$i]['count']=$row['browsed'];
 	$arr['buy'][$i]['time']=$row['created_time'];
 	$arr['buy'][$i]['nickname']=$row['nickname'];
+	$arr['buy'][$i]['commentNum']=$row['comments'];
 	//$arr['buy'][$i]['img']=$row['picture'];
 	$arr['buy'][$i]['title']=$row['title'];
 	$i += 1;
 }
 $arr['passage']['buy']=$i;
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='job' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='job' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -67,12 +69,14 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['job'][$i]['count']=$row['browsed'];
 	$arr['job'][$i]['time']=$row['created_time'];
 	$arr['job'][$i]['nickname']=$row['nickname'];
+	$arr['job'][$i]['commentNum']=$row['comments'];
+
 	//$arr['job'][$i]['img']=$row['picture'];
 	$arr['job'][$i]['title']=$row['title'];
 	$i += 1;
 }
 $arr['passage']['job']=$i;
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='other' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='other' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -83,12 +87,14 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['other'][$i]['count']=$row['browsed'];
 	$arr['other'][$i]['time']=$row['created_time'];
 	$arr['other'][$i]['nickname']=$row['nickname'];
+	$arr['other'][$i]['commentNum']=$row['comments'];
+
 	//$arr['other'][$i]['img']=$row['picture'];
 	$arr['other'][$i]['title']=$row['title'];
 	$i += 1;
 }
 $arr['passage']['other']=$i;
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='more' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='more' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -99,12 +105,14 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['more'][$i]['count']=$row['browsed'];
 	$arr['more'][$i]['time']=$row['created_time'];
 	$arr['more'][$i]['nickname']=$row['nickname'];
+	$arr['more'][$i]['commentNum']=$row['comments'];
+
 	//$arr['more'][$i]['img']=$row['picture'];
 	$arr['more'][$i]['title']=$row['title'];
 	$i += 1;
 }
 $arr['passage']['more']=$i;
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='restaurant' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='restaurant' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -115,12 +123,14 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['restaurant'][$i]['count']=$row['browsed'];
 	$arr['restaurant'][$i]['time']=$row['created_time'];
 	$arr['restaurant'][$i]['nickname']=$row['nickname'];
+	$arr['restaurant'][$i]['commentNum']=$row['comments'];
+
 	//$arr['restaurant'][$i]['img']=$row['picture'];
 	$arr['restaurant'][$i]['title']=$row['title'];
 	$i += 1;
 }
 $arr['passage']['restaurant']=$i;
-$sql = "SELECT title,serial,created_time,nickname,browsed FROM `fduroot_article` WHERE category='house' ORDER BY created_time DESC limit 0,4";
+$sql = "SELECT title,serial,created_time,nickname,browsed,comments FROM `fduroot_article` WHERE category='house' ORDER BY created_time DESC limit 0,4";
 $retval = mysqli_query($conn,$sql);
 //$arr = array();
 $i = 0;
@@ -131,6 +141,8 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$arr['house'][$i]['count']=$row['browsed'];
 	$arr['house'][$i]['time']=$row['created_time'];
 	$arr['house'][$i]['nickname']=$row['nickname'];
+	$arr['house'][$i]['commentNum']=$row['comments'];
+
 	//$arr['house'][$i]['img']=$row['picture'];
 	$arr['house'][$i]['title']=$row['title'];
 	$i += 1;
